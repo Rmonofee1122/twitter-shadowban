13683c2dfbf8b61f25c4675a8557f3b6c57cfa25
c7e715d1b04b17683718fb1e8944cc28 <154405627+c7e715d1b04b17683718fb1e8944cc28@users.noreply.github.com>
2024-02-28 22:44:05 +0900

    Feature: Test Part

--- src/app/api/[[...slug]]/route.ts
+++ src/app/api/[[...slug]]/route.ts
@@ -1,15 +1,252 @@
-import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { swaggerUI } from "@hono/swagger-ui";
+import { ResultsSchema, TestQuerySchema } from "./schemas";
+import ky from "ky";
+
+function generateRandomHexString(length: number) {
+  let result = "";
+  const characters = "0123456789abcdef";
+  for (let i = 0; i < length * 2; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+const app = new OpenAPIHono().basePath("/api");
 
 app.use(logger());
 
-app.get("*", async (c) => {
-  return c.text(process.env.AUTH_TOKEN ?? '');
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "X (Twitter) Shadowban Test API",
+  },
+  servers: [
+    {
+      url: "/api",
+    },
+  ],
 });
+
+app.get(
+  "/docs",
+  swaggerUI({
+    url: "/api/openapi.json",
+  }),
+);
+
+const route = app.openapi(
+  createRoute({
+    method: "get",
+    path: "/test",
+    request: {
+      query: TestQuerySchema,
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: ResultsSchema,
+          },
+        },
+        description: "成功",
+      },
+    },
+  }),
+  async (c) => {
+    const authToken = process.env.AUTH_TOKEN;
+    if (!authToken) {
+      throw new Error("AUTH_TOKEN is not defined");
+    }
+    const csrfToken = generateRandomHexString(16);
+    const client = ky.create({
+      prefixUrl: "https://api.twitter.com",
+      headers: {
+        Authorization:
+          "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+        Cookie: `auth_token=${authToken}; ct0=${csrfToken}`,
+        "X-Csrf-Token": csrfToken,
+      },
+    });
+    const { screen_name: screenName } = c.req.valid("query");
+    const { data: { user } } = await client.get(
+      "graphql/k5XapwcSikNsEsILW5FvgA/UserByScreenName",
+      {
+        searchParams: {
+          "variables": JSON.stringify({
+            "screen_name": screenName,
+            "withSafetyModeUserFields": true,
+          }),
+          "features": JSON.stringify({
+            "hidden_profile_likes_enabled": true,
+            "hidden_profile_subscriptions_enabled": true,
+            "responsive_web_graphql_exclude_directive_enabled": true,
+            "verified_phone_label_enabled": false,
+            "subscriptions_verification_info_is_identity_verified_enabled":
+              true,
+            "subscriptions_verification_info_verified_since_enabled": true,
+            "highlights_tweets_tab_ui_enabled": true,
+            "responsive_web_twitter_article_notes_tab_enabled": true,
+            "creator_subscriptions_tweet_preview_api_enabled": true,
+            "responsive_web_graphql_skip_user_profile_image_extensions_enabled":
+              false,
+            "responsive_web_graphql_timeline_navigation_enabled": true,
+          }),
+          "fieldToggles": JSON.stringify({
+            "withAuxiliaryUserLabels": false,
+          }),
+        },
+      },
+    ).json<any>();
+    if (!user) {
+      return c.json({
+        not_found: true,
+        suspend: false,
+        protect: false,
+        no_tweet: false,
+        search_ban: false,
+        search_suggestion_ban: false,
+        no_reply: false,
+        ghost_ban: false,
+        reply_deboosting: false,
+        user: null,
+      });
+    }
+    if (user.result.__typename !== "User") {
+      return c.json({
+        not_found: false,
+        suspend: true,
+        protect: false,
+        no_tweet: false,
+        search_ban: false,
+        search_suggestion_ban: false,
+        no_reply: false,
+        ghost_ban: false,
+        reply_deboosting: false,
+        user: user.result,
+      });
+    }
+    if (user.result.legacy.protected) {
+      return c.json({
+        not_found: false,
+        suspend: false,
+        protect: true,
+        no_tweet: false,
+        search_ban: false,
+        search_suggestion_ban: false,
+        no_reply: false,
+        ghost_ban: false,
+        reply_deboosting: false,
+        user: user.result,
+      });
+    }
+    if (!user.result.legacy.statuses_count) {
+      return c.json({
+        not_found: false,
+        suspend: false,
+        protect: false,
+        no_tweet: true,
+        search_ban: false,
+        search_suggestion_ban: false,
+        no_reply: false,
+        ghost_ban: false,
+        reply_deboosting: false,
+        user: user.result,
+      });
+    }
+    const {
+      data: { search_by_raw_query: { search_timeline: searchTimeline } },
+    } = await client.get(
+      "graphql/3k6tjrexrMMfvGkBm7wDZg/SearchTimeline",
+      {
+        searchParams: {
+          "variables": JSON.stringify({
+            "rawQuery": `from:${user.result.legacy.screen_name}`,
+            "count": 20,
+            "querySource": "typed_query",
+            "product": "Top",
+          }),
+          "features": JSON.stringify({
+            "responsive_web_graphql_exclude_directive_enabled": true,
+            "verified_phone_label_enabled": false,
+            "creator_subscriptions_tweet_preview_api_enabled": true,
+            "responsive_web_graphql_timeline_navigation_enabled": true,
+            "responsive_web_graphql_skip_user_profile_image_extensions_enabled":
+              false,
+            "c9s_tweet_anatomy_moderator_badge_enabled": true,
+            "tweetypie_unmention_optimization_enabled": true,
+            "responsive_web_edit_tweet_api_enabled": true,
+            "graphql_is_translatable_rweb_tweet_is_translatable_enabled": true,
+            "view_counts_everywhere_api_enabled": true,
+            "longform_notetweets_consumption_enabled": true,
+            "responsive_web_twitter_article_tweet_consumption_enabled": true,
+            "tweet_awards_web_tipping_enabled": false,
+            "freedom_of_speech_not_reach_fetch_enabled": true,
+            "standardized_nudges_misinfo": true,
+            "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":
+              true,
+            "rweb_video_timestamps_enabled": true,
+            "longform_notetweets_rich_text_read_enabled": true,
+            "longform_notetweets_inline_media_enabled": true,
+            "responsive_web_enhance_cards_enabled": false,
+          }),
+        },
+      },
+    ).json<any>();
+    let searchBanFlag = true;
+    for (const instruction of searchTimeline.timeline.instructions) {
+      for (const entry of instruction.entries) {
+        if (entry.entryId.startsWith("tweet-")) {
+          if (
+            entry.content.itemContent.tweet_results.result.core.user_results
+              .result.legacy.screen_name === user.result.legacy.screen_name
+          ) {
+            searchBanFlag = false;
+            break;
+          }
+        }
+      }
+    }
+    const { users: searchSuggestionUsers } = await client.get(
+      "1.1/search/typeahead.json",
+      {
+        searchParams: {
+          "include_ext_is_blue_verified": "1",
+          "include_ext_verified_type": "1",
+          "include_ext_profile_image_shape": "1",
+          "q": `@${user.result.legacy.screen_name} ${user.result.legacy.name}`,
+          "src": "search_box",
+          "result_type": "events,users,topics,lists",
+        },
+      },
+    ).json<any>();
+    let searchSuggestionBanFlag = true;
+    for (const searchSuggestionUser of searchSuggestionUsers) {
+      if (searchSuggestionUser.screen_name === user.result.legacy.screen_name) {
+        searchSuggestionBanFlag = false;
+        break;
+      }
+    }
+    return c.json({
+      not_found: false,
+      suspend: false,
+      protect: false,
+      no_tweet: false,
+      search_ban: searchBanFlag,
+      search_suggestion_ban: searchSuggestionBanFlag,
+      no_reply: false,
+      ghost_ban: false,
+      reply_deboosting: false,
+      user: user.result,
+    });
+  },
+);
+
+export type AppType = typeof route;
 
 export const GET = app.fetch;
 export const POST = app.fetch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import { Accordion, AccordionItem, Button, Card, CardBody, CardHeader, Divider, Input } from "@nextui-org/react";
+import { Accordion, AccordionItem, Avatar, Button, Card, CardBody, CardHeader, Divider, Input } from "@nextui-org/react";
 import { Montserrat } from "next/font/google";
-import { FaAt, FaMagnifyingGlass, FaQuestion } from "react-icons/fa6";
+import { useState } from "react";
+import { FaAt, FaBan, FaCheck, FaMagnifyingGlass, FaQuestion } from "react-icons/fa6";
+import { hc } from "hono/client";
+import { ResultsSchema } from "./api/[[...slug]]/schemas";
+import { AppType } from "./api/[[...slug]]/route";
+import { z } from "@hono/zod-openapi";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [results, setResults] = useState<z.infer<typeof ResultsSchema>>();
+  const client = hc<AppType>('/api');
+  const test = async (screenName: string) => {
+    const results = await client.test.$get({
+      query: {
+        screen_name: screenName,
+      },
+    });
+    setResults(await results.json());
+  }
   return (
     <div className="flex flex-col items-center justify-center my-12">
       <div className="flex flex-col items-center justify-center max-w-4xl px-4 gap-8">
         <h1 className={`${montserrat.className} text-4xl text-bold`}>
-          Is @Username<br />Shadowbanned on X (Twitter)?
+          Is @{username ? username : 'Username'}<br />Shadowbanned on X (Twitter)?
         </h1>
         <Card className="dark:bg-neutral-800 w-full p-4">
           <CardBody>
             <div className="flex">
-              <Input radius="full" type="text" color="primary" placeholder="X" labelPlacement="outside" startContent={<FaAt />} />
-              <Button isIconOnly radius="full" color="primary" className="ml-2">
+              <Input radius="full" type="text" color="primary" placeholder="X" labelPlacement="outside" startContent={<FaAt />} onChange={(e) => setUsername(e.target.value)} />
+              <Button isIconOnly radius="full" color="primary" className="ml-2" onClick={() => test(username)}>
                 <FaMagnifyingGlass />
               </Button>
             </div>
@@ -26,29 +42,84 @@ export default function Home() {
         <Divider />
         <Card className="dark:bg-neutral-800 w-full">
           <CardHeader>
-            <h2 className="text-bold">シャドウバンとは？</h2>
+            {results ? results.user ? results.user.__typename === 'User' ? (
+              <div className="flex gap-2">
+                <Avatar size="lg" src={results.user.legacy.profile_image_url_https} />
+                <div>
+                  <h2 className="text-lg text-bold">{results.user.legacy.name}</h2>
+                  <p>@{results.user.legacy.screen_name}</p>
+                </div>
+              </div>
+            ) : (
+              <h2 className="text-bold">ユーザーが凍結されています。</h2>
+            ) : (
+              <h2 className="text-bold">ユーザーが見つかりませんでした。</h2>
+            ) : (
+              <h2 className="text-bold">シャドウバンとは？</h2>
+            )}
           </CardHeader>
           <div className="px-4">
             <Divider />
           </div>
           <CardBody>
-            <p>シャドウバンとは、X (Twitter) において、アカウントがロックや凍結されていないにも関わらず、検索結果や返信一覧に表示されなく(ずらく)なる状態のことです。</p>
+            {results ? results.user ? results.user.__typename === 'User' ? (
+              <p>{results.user.legacy.description}</p>
+            ) : (
+              <p>ユーザー情報にアクセスできませんでした、ユーザーが凍結されているかもしれません</p>
+            ) : (
+              <p>ユーザー情報にアクセスできませんでした、ユーザーが見つかりませんでした</p>
+            ) : (
+              <p>シャドウバンとは、X (Twitter) において、アカウントがロックや凍結されていないにも関わらず、検索結果や返信一覧に表示されなく(ずらく)なる状態のことです。</p>
+            )}
+
           </CardBody>
         </Card>
         <Accordion variant="shadow" className="dark:bg-neutral-800 w-full">
-          <AccordionItem key="1" aria-label="Search Suggestion Ban" title="Search Suggestion Ban" startContent={<FaQuestion className="text-xl text-blue-500" />}>
+          <AccordionItem key="1" aria-label="Search Suggestion Ban" title="Search Suggestion Ban" startContent={
+            results && results.user && results.user.__typename === 'User' && !results.protect && !results.no_tweet ? results.search_suggestion_ban ? (
+              <FaBan className="text-xl text-red-500" />
+            ) : (
+              <FaCheck className="text-xl text-green-500" />
+            ) : (
+              <FaQuestion className="text-xl text-blue-500" />
+            )
+          }>
             <h3 className="text-xl text-bold">検索候補からの除外</h3>
             <p>検索画面において、検索候補から該当のアカウントが表示されなくなります。</p>
           </AccordionItem>
-          <AccordionItem key="2" aria-label="Search Ban" title="Search Ban" startContent={<FaQuestion className="text-xl text-blue-500" />}>
+          <AccordionItem key="2" aria-label="Search Ban" title="Search Ban" startContent={
+            results && results.user && results.user.__typename === 'User' && !results.protect && !results.no_tweet ? results.search_ban ? (
+              <FaBan className="text-xl text-red-500" />
+            ) : (
+              <FaCheck className="text-xl text-green-500" />
+            ) : (
+              <FaQuestion className="text-xl text-blue-500" />
+            )
+          }>
             <h3 className="text-xl text-bold">検索結果からの除外</h3>
             <p>検索結果から、該当のアカウントのツイートやアカウントが表示されなくなります。</p>
           </AccordionItem>
-          <AccordionItem key="3" aria-label="Ghost Ban" title="Ghost Ban" startContent={<FaQuestion className="text-xl text-blue-500" />}>
+          <AccordionItem key="3" aria-label="Ghost Ban" title="Ghost Ban" startContent={
+            results && results.user && results.user.__typename === 'User' && !results.protect && !results.no_tweet && !results.no_reply ? results.ghost_ban ? (
+              <FaBan className="text-xl text-red-500" />
+            ) : (
+              <FaCheck className="text-xl text-green-500" />
+            ) : (
+              <FaQuestion className="text-xl text-blue-500" />
+            )
+          }>
             <h3 className="text-xl text-bold">返信一覧からの除外</h3>
             <p>ポストに対する返信が返信一覧から表示されなくなり、ポスト投稿主への通知もされなくなります。</p>
           </AccordionItem>
-          <AccordionItem key="4" aria-label="Reply Deboosting" title="Reply Deboosting" startContent={<FaQuestion className="text-xl text-blue-500" />}>
+          <AccordionItem key="4" aria-label="Reply Deboosting" title="Reply Deboosting" startContent={
+            results && results.user && results.user.__typename === 'User' && !results.protect && !results.no_tweet && !results.no_reply ? results.reply_deboosting ? (
+              <FaBan className="text-xl text-red-500" />
+            ) : (
+              <FaCheck className="text-xl text-green-500" />
+            ) : (
+              <FaQuestion className="text-xl text-blue-500" />
+            )
+          }>
             <h3 className="text-xl text-bold">返信一覧での表示順の低下</h3>
             <p>ポストに対する返信が返信一覧にて、下部に表示されるようになります。<br />また、「さらに返信を表示する」をタップするまで返信が表示されなくなる場合があります。</p>
           </AccordionItem>
